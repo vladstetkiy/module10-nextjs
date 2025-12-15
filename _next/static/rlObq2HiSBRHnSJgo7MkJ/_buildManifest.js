f1183ca768c0d2b3becfcbeac85df85086db6db6
stetskiy_Vlad <vladworkprog@gmail.com>
2025-12-15 14:14:29 +0300

    Updates

---
+++ _next/static/rlObq2HiSBRHnSJgo7MkJ/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [
+      {
+        "source": "/module10-nextjs/module10-nextjs/mockServiceWorker.js",
+        "destination": "/module10-nextjs/mockServiceWorker.js"
+      }
+    ],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
