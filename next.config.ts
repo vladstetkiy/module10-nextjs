70245041882bdb3eb499398a183b3c1f52e728ac
stetskiy_Vlad <vladworkprog@gmail.com>
2025-12-15 18:05:55 +0300

    fix: updated next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,23 @@
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
+
+  async rewrites() {
+    return [
+      {
+        source: '/Module10/mockServiceWorker.js',
+        destination: '/mockServiceWorker.js',
+      },
+    ];
+  },
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: ['@svgr/webpack'],
+        as: '*.js',
+      },
+    },
+  },
 };
 
 module.exports = nextConfig;
